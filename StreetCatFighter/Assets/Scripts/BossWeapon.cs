ab3475f0635337d43c0ae47343432758e61dbd3c
kangyeokyung <hanaemi1322412@naver.com>
2022-12-05 01:40:53 +0900

    배틀씬(체력회복아이템, 플레이어체력바, 공격패턴 일부)

---
+++ StreetCatFighter/Assets/Scripts/BossWeapon.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AttackType {  CircleFire = 0, }
+public class BossWeapon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemybulletprefab;//공격힐때 발사체 프리팹
+
+    public void StartFiring(AttackType attackType)
+    {
+        StartCoroutine(attackType.ToString());
+    }
+
+    public void StopFiring(AttackType attackType)
+    {
+
+        StopCoroutine(attackType.ToString());
+    }
+
+    private IEnumerator CircleFire()
+    {
+        float attackRate = 0.5f;//공격주기
+        int count = 10;//발사체 생성 개수
+        float intervalAngle = 360 / count;//발사체 사이의 각도
+        float weightAngle = 0;//가중되는 각도
+
+        while (true)//원 형태로 방사하는 발사체
+        {
+            for(int i = 0; i <count; ++i)
+            {
+                GameObject clone = Instantiate(enemybulletprefab, transform.position, Quaternion.identity);
+                float angle = weightAngle + intervalAngle * i;//발사체 이동 방향 -각도
+                float x = Mathf.Cos(angle * Mathf.PI / 180.0f);//발사체 이동 방향 - 벡터
+                float y = Mathf.Sin(angle * Mathf.PI / 180.0f);
+                clone.GetComponent<Movement2d>().MoveTo(new Vector2(x, y));//발사체 이동방향
+            }
+
+            weightAngle += 1;//발사체 생성 시작 각도
+
+            yield return new WaitForSeconds(attackRate);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartFiring(AttackType.CircleFire);
+    }
+}
